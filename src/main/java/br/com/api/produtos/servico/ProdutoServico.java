0eaefc5a43b4136ac2afa39906e06dffa8de77f8
lucas51512 <lucasbrancalhao454@gmail.com>
2023-01-20 15:48:33 -0300

    Método create finalizado

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,41 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
+import br.com.api.produtos.respositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
-    
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    @Autowired
+    private RespostaModelo rm;
+
+    // Método para listar todos os produtos
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
+
+    // Método para cadastrar ou alterar produtos
+    public ResponseEntity<?> cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setMensagem("O nome da marca é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else {
+            if (acao.equals("cadastrar")) {
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+            }
+        }
+    }
 }
